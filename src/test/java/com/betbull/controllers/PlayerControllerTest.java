0362d6ea80c72456cea21981e71fc45025ec2a4d
Yuriy Movchan <67926237+yuriymo@users.noreply.github.com>
2020-11-24 01:34:34 +0200

    add Player

--- src/test/java/com/betbull/controllers/PlayerControllerTest.java
+++ src/test/java/com/betbull/controllers/PlayerControllerTest.java
@@ -3,8 +3,6 @@ package com.betbull.controllers;
 import com.betbull.Application;
 import com.betbull.dto.PlayerDto;
 import com.betbull.dto.TeamDto;
-import com.betbull.services.PlayerService;
-import com.betbull.services.TeamService;
 import com.google.gson.Gson;
 import lombok.val;
 import org.junit.Test;
@@ -29,23 +27,23 @@ public class PlayerControllerTest {
     @Test
     public void addPlayer() throws Exception {
 
-        val teamDto = new Gson().fromJson(getResponseAsString("/add-player/test"), TeamDto.class);
+        val teamDto = new Gson().fromJson(retrieveResponseAsString("/add-player/test"), TeamDto.class);
         if (isNull(teamDto)) {
             Assertions.fail("add-team - fail");
         }
 
-        val playerDto = new Gson().fromJson(getResponseAsString("/add-player?name=player-1&teamId=" + teamDto.getId()), PlayerDto.class);
+        val playerDto = new Gson().fromJson(retrieveResponseAsString("/add-player?name=player-1&teamId=" + teamDto.getId()), PlayerDto.class);
         if (isNull(playerDto)) {
             Assertions.fail("add-player - fail");
         }
 
-        val playerDto2 = new Gson().fromJson(getResponseAsString("/player/" + playerDto.getId()), PlayerDto.class);
+        val playerDto2 = new Gson().fromJson(retrieveResponseAsString("/player/" + playerDto.getId()), PlayerDto.class);
         if (isNull(playerDto2)) {
             Assertions.fail("get-player - fail");
         }
     }
 
-    private String getResponseAsString(String s) throws Exception {
+    private String retrieveResponseAsString(String s) throws Exception {
         return mvc.perform(MockMvcRequestBuilders.get(s)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
