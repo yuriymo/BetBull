20201db98fb45532982e1955c78399233b7e42b8
yurimovchan <yurimo.dev@gmael.com>
2020-11-25 00:39:20 +0200

    fix PlayerControllerTest

--- src/test/java/com/betbull/controller/PlayerControllerTest.java
+++ src/test/java/com/betbull/controller/PlayerControllerTest.java
@@ -1,23 +1,25 @@
 package com.betbull.controller;
 
-import com.betbull.Application;
 import com.betbull.model.PlayerDto;
 import com.betbull.model.TeamDto;
 import com.google.gson.Gson;
 import lombok.val;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static java.util.Objects.isNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = Application.class)
+@SpringBootTest
+@RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 public class PlayerControllerTest {
 
